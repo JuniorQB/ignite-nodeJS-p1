4efac7a5251be11f40c8572d472b983b08441e3e
JuniorQb <eucomcerteza@gmail.com>
2024-03-14 23:02:27 -0300

    test route

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 import http from 'node:http'
 
 
+
+
+
 const server = http.createServer((request, response) => {
+  const {method, url } = request
+  if(method === 'GET' && url === "/users"){
+    console.log('Lista de Usuarios')
+  }
+
   return response.end(
     'Hello World'
   )
